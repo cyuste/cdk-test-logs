0057e8aabe5409ff5e92f231775f866b51eaef81
Carlos Yuste <carlos.yuste@col.vueling.com>
2020-09-09 13:53:18 +0200

    inital commit

--- lib/cdk-test-logs-stack.ts
+++ lib/cdk-test-logs-stack.ts
@@ -1,18 +1,25 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
+import * as kinesis from '@aws-cdk/aws-kinesis';
+import * as logs from '@aws-cdk/aws-logs';
+import * as destinations from '@aws-cdk/aws-logs-destinations';
 import * as cdk from '@aws-cdk/core';
+
 
 export class CdkTestLogsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkTestLogsQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const log = new logs.LogGroup(this, "xx", {
+      logGroupName : "logname",
+      retention : logs.RetentionDays.TWO_WEEKS
+    });
+    
+    const importedStream = kinesis.Stream.fromStreamArn(this, "kinesis-stream", "arn:aws:logs:eu-west-1:67777777777:destination:app-log-pre-destination");
+
+    const subscription = new logs.SubscriptionFilter(this, "subs-filter", {
+      destination : new destinations.KinesisDestination(importedStream),
+      logGroup: log,
+      filterPattern: logs.FilterPattern.anyTerm()
     });
 
-    const topic = new sns.Topic(this, 'CdkTestLogsTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
